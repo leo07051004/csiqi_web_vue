cbd23c67293b8598a23dd06c5fbd5a4118f5c62f
leo07051004 <975121990@qq.com>
2019-11-21 17:51:31 +0800

    云 和 本地 ip 切换

--- javalsj-vue/src/components/common/BlogFooter.vue
+++ javalsj-vue/src/components/common/BlogFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="text_center">
-    csiqi.cn
+    <p id="cp">©2019&nbsp;&nbsp;
+      &nbsp;浙ICP备19048058号&nbsp;<i class="c-icon-icrlogo"></i>&nbsp;
+      <a id="jgwab" target="_blank" href="http://www.beian.gov.cn">
+        浙ICP备19048058号</a>&nbsp;<i class="c-icon-jgwablogo"></i></p>
+    <!--csiqi.cn-->
   </div>
 </template>
 <style>
